26ff0f9d8e19b654ba59311850c465247b24425e
TomazMartins <tomaz.unb@outlook.com>
2015-05-13 19:11:02 -0300

    Adding configuration about the new tool that it will manage data of the tests
    Signed-off-by: TomazMartins <tomaz.unb@outlook.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,5 +25,21 @@ RSpec.configure do |config|
   #
   config.use_transactional_fixtures = false
 
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   config.infer_spec_type_from_file_location!
 end
